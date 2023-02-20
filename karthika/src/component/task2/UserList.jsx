d7ef857a9cea0690302a331b8b85bea5f12403da
Karthika J <Karthika.J@gds.ey.com>
2023-02-20 16:18:39 +0530

    task 2

--- karthika/src/component/task2/UserList.jsx
+++ karthika/src/component/task2/UserList.jsx
@@ -3,16 +3,18 @@ const UserList = (props) => {
     const { users, isLoggedIn } = props;
     return isLoggedIn ? (
         <table border="1">
-           
+           <tr><th>ID</th><th>Name</th><th>Age</th><th>Email</th></tr>
             {
                 users.map((user) => {
+                   
                     return (
                         <>
-                         <tr><th>ID</th><th>Name</th><th>Age</th><th>Email</th></tr>
+                         <tr>
                         <td key={user.id}>{user.id}</td>
                         <td >{user.name}</td>
                         <td >{user.age}</td>
                         <td >{user.email}</td>
+                        </tr>
                         </>
                         
                     );
